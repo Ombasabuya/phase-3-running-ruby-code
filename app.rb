45c0df75f946dd17fc114104000a73d736bde0f1
ombasaabuya <ombasabuya@gmail.com>
2023-02-06 23:21:59 +0300

    completed

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 # app.rb
 puts "Hello World!"
-print "Pass this test,please."
+
+# app.rb
+print "Pass this test, please."
+
+# app.rb
 p [1,2,3]
-puts [1,nil,nil,4]
+
+# # app.rb
+# puts [1,nil,nil,4]
